b729907b5eabdf1a5fc768c1f36f228324f3a8e0
Aditya Prashar <aditya@Adityas-MacBook-Air.local>
2025-07-22 12:17:55 +0530

    added constant

--- app/src/main/java/com/example/anime/model/api/AuthInterceptor.kt
+++ app/src/main/java/com/example/anime/model/api/AuthInterceptor.kt
@@ -1,12 +1,13 @@
 package com.example.anime.model.api
 
+import com.example.anime.util.Constant
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthTokenInterceptor : Interceptor{
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalReq = chain.request()
-        val reqBuilder = originalReq.newBuilder().header("X-MAL-CLIENT-ID","a8e97fa29f95ada3168ae089d8be965b")
+        val reqBuilder = originalReq.newBuilder().header("X-MAL-CLIENT-ID", Constant.CLIENT_ID)
         val request = reqBuilder.build()
         return chain.proceed(request)
     }
---
+++ app/src/main/java/com/example/anime/util/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.anime.util
+
+object Constant {
+    const val CLIENT_ID = "a8e97fa29f95ada3168ae089d8be965b"
+}
